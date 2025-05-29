82ed9d6b43f2d5fd6a2f3b53ba48fc0b43b14999
PatNwk <nowakp595@gmail.com>
2025-05-29 21:43:03 +0200

    Debug bdd

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { PitchDeck } from '../pitch-deck/pitch-deck.entity';
+import { Comment } from '../comment/comment.entity';
+import { Message } from '../message/message.entity';
+import { Favorite } from '../favorite/favorite.entity';
+import { Notification } from '../notification/notification.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      PitchDeck,
+      Comment,
+      Message,
+      Favorite,
+      Notification,
+    ]),
+  ],
   providers: [UserService],
   controllers: [UserController],
-  exports: [UserService]
+  exports: [UserService],
 })
 export class UserModule {}
